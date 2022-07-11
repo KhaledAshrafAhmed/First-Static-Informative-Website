f4159b9cafc36538203f94136dfc3398181d9c84
KhaledAshrafAhmed <104087451+KhaledAshrafAhmed@users.noreply.github.com>
2022-07-11 16:10:00 +0200

    Add files via upload

---
+++ DICOM Website/scroll.js
@@ -0,0 +1,17 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight; // by get el inner height bta3 el view port
+      var elementTop = reveals[i].getBoundingClientRect().top; //by get el size bta3 el element bel nesba l el viewport
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
